2821cda27e338e5da4f167e4fb2421fd5c87a2f1
gabvteixeira <gabriel.vteixeira@mercadolivre.com>
2022-10-05 13:02:55 -0300

    wip: Purchase controller

---
+++ src/main/java/br/com/equipe7/desafio_spring/controller/PurchaseController.java
@@ -0,0 +1,27 @@
+package br.com.equipe7.desafio_spring.controller;
+
+import br.com.equipe7.desafio_spring.dto.PurchaseRequestDTO;
+import br.com.equipe7.desafio_spring.dto.TicketResponseDTO;
+import br.com.equipe7.desafio_spring.service.IProduct;
+import br.com.equipe7.desafio_spring.service.IPurchase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class PurchaseController {
+
+    @Autowired
+    private IPurchase service;
+
+    @PostMapping("/purchase-request")
+    public ResponseEntity<TicketResponseDTO> purchase(@RequestBody PurchaseRequestDTO request){
+        TicketResponseDTO response = this.service.purchase(request);
+        return new ResponseEntity(response, HttpStatus.OK);
+    }
+}
