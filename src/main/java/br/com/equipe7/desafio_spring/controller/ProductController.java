8552f3e9f353ebc70ae9e76022669ab006bf8f91
fyshinkae <felipe.shinkae@mercadolivre.com>
2022-10-05 10:37:51 -0400

    Feat: handle JSON file added to project

--- src/main/java/br/com/equipe7/desafio_spring/controller/ProductController.java
+++ src/main/java/br/com/equipe7/desafio_spring/controller/ProductController.java
@@ -1,7 +1,20 @@
 package br.com.equipe7.desafio_spring.controller;
 
+import br.com.equipe7.desafio_spring.model.Product;
+import br.com.equipe7.desafio_spring.repository.ProductRepo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
+
+    @GetMapping("/api/v1/articles")
+    public ResponseEntity<List<Product>> getAll() {
+        ProductRepo productList = new ProductRepo();
+        return new ResponseEntity<>(productList.getProductList(), HttpStatus.OK);
+    }
 }
