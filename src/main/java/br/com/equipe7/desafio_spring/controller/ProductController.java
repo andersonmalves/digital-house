bfa4b4f0c401f9aba2ae8cee5057b1e6a0ff1b61
gabvteixeira <gabriel.vteixeira@mercadolivre.com>
2022-10-05 10:57:49 -0300

    add autowired in controller and service

--- src/main/java/br/com/equipe7/desafio_spring/controller/ProductController.java
+++ src/main/java/br/com/equipe7/desafio_spring/controller/ProductController.java
@@ -1,7 +1,16 @@
 package br.com.equipe7.desafio_spring.controller;
 
+import br.com.equipe7.desafio_spring.service.IProduct;
+import br.com.equipe7.desafio_spring.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class ProductController {
+    
+    @Autowired
+    private IProduct service;
+
 }
