6a5edc6ff3e4872ec5b070d80a6e18204260c335
Matheus Ferreira <matheus.aferreira@mercadolivre.com>
2022-10-06 14:56:23 -0300

    refact: AllArgsConstructor notation added in DTO ProductCreatedDTO

--- src/main/java/br/com/equipe7/desafio_spring/dto/ProductCreatedDTO.java
+++ src/main/java/br/com/equipe7/desafio_spring/dto/ProductCreatedDTO.java
@@ -8,30 +8,10 @@ import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class ProductCreatedDTO {
     private String name, category, brand, prestige;
     private BigDecimal price;
     private boolean freeShipping;
     private int quantity;
-
-    /**
-     * @author Giovanna
-     * @param name
-     * @param category
-     * @param brand
-     * @param prestige
-     * @param price
-     * @param freeShipping
-     * @param quantity
-     */
-    public ProductCreatedDTO(String name, String category, String brand, String prestige,
-                             BigDecimal price, boolean freeShipping, int quantity) {
-        this.name = name;
-        this.category = category;
-        this.brand = brand;
-        this.prestige = prestige;
-        this.price = price;
-        this.freeShipping = freeShipping;
-        this.quantity = quantity;
-    }
 }
