26969ad9926865aa00206149e6b363cf6e5eb9f0
Matheus Ferreira <matheus.aferreira@mercadolivre.com>
2022-10-13 17:01:46 -0300

    feat: getArea implemented

--- src/main/java/com/example/desafio_quality/interfaces/IPropertyService.java
+++ src/main/java/com/example/desafio_quality/interfaces/IPropertyService.java
@@ -6,7 +6,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public interface IPropertyService {
-    double getArea();
+    double getArea(int id);
     BigDecimal getValue();
     List<Room> getRooms();
     Room getBiggestRoom();
