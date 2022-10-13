e8e8a0e82b0115b3d510cf6ea0cc38613e70a41a
Matheus Ferreira <matheus.aferreira@mercadolivre.com>
2022-10-13 16:52:12 -0300

    feat: getPropertyById create in Repository

---
+++ src/main/java/com/example/desafio_quality/repository/PropertyRepo.java
@@ -0,0 +1,17 @@
+package com.example.desafio_quality.repository;
+
+import com.example.desafio_quality.entity.Property;
+import com.example.desafio_quality.util.ManipulateFile;
+
+import java.util.List;
+import java.util.Optional;
+
+public class PropertyRepo {
+    public Optional<Property> getPropertyById(int id) {
+        List<Property> properties = ManipulateFile.loadProperties();
+
+        return properties.stream()
+                .filter(p -> p.getPropId() == id)
+                .findFirst();
+    }
+}
