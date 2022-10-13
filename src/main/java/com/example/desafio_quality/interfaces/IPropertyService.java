6cb768502fac61e68fcb8ace6709d686624780fc
Matheus Ferreira <matheus.aferreira@mercadolivre.com>
2022-10-13 15:47:40 -0300

    feat: created interface IPropertyService

---
+++ src/main/java/com/example/desafio_quality/interfaces/IPropertyService.java
@@ -0,0 +1,13 @@
+package com.example.desafio_quality.interfaces;
+
+import com.example.desafio_quality.entity.Room;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface IPropertyService {
+    double getArea();
+    BigDecimal getValue();
+    List<Room> getRooms();
+    Room getBiggestRoom();
+}
