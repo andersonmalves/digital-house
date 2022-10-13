66f358369722134608579dfa9b087957dd02a345
fyshinkae <felipe.shinkae@mercadolivre.com>
2022-10-13 16:00:34 -0400

    Feat: Created rooms controller

--- src/main/java/com/example/desafio_quality/controller/PropertyController.java
+++ src/main/java/com/example/desafio_quality/controller/PropertyController.java
@@ -1,9 +1,44 @@
 package com.example.desafio_quality.controller;
 
+import com.example.desafio_quality.dto.RoomDTO;
+import com.example.desafio_quality.entity.Room;
+import com.example.desafio_quality.interfaces.IPropertyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/properties")
 public class PropertyController {
+    @Autowired
+    private IPropertyService service;
+
+    /**
+     * Controller para todos os quartos
+     * @author Felipe, Gabriel
+     * @param propId ‘ID’ da propriedade
+     * @return HTTP status e todos quartos
+     */
+    @GetMapping("/rooms/{propId}")
+    public ResponseEntity<List<Room>> getAllRooms(@PathVariable int propId) {
+        List<Room> rooms = service.getRooms();
+        return ResponseEntity.ok(rooms);
+    }
+
+    /**
+     * Controller para o maior quarto
+     * @author Felipe, Gabriel
+     * @param propId ‘ID’ da propriedade
+     * @return HTTP status e maior quarto e sua area quadrada
+     */
+    @GetMapping("biggest-room/{propId}")
+    public ResponseEntity<RoomDTO> getBiggestRoom(@PathVariable int propId) {
+        Room room = service.getBiggestRoom();
+        return new ResponseEntity.ok(new RoomDTO(room));
+    }
 }
---
+++ src/main/java/com/example/desafio_quality/service/PropertyService.java
@@ -0,0 +1,33 @@
+package com.example.desafio_quality.service;
+
+import com.example.desafio_quality.entity.Room;
+import com.example.desafio_quality.interfaces.IPropertyService;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class PropertyService implements IPropertyService {
+
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
+
+    @Override
+    public BigDecimal getValue() {
+        return null;
+    }
+
+    @Override
+    public List<Room> getRooms() {
+        return null;
+    }
+
+    @Override
+    public Room getBiggestRoom() {
+        return null;
+    }
+}
