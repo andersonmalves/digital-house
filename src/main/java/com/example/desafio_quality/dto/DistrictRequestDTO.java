ba71a57194760a36efc47f1954bcc9daae7e7dd4
fyshinkae <felipe.shinkae@mercadolivre.com>
2022-10-14 17:20:38 -0400

    feat: added validations

--- src/main/java/com/example/desafio_quality/dto/DistrictRequestDTO.java
+++ src/main/java/com/example/desafio_quality/dto/DistrictRequestDTO.java
@@ -4,12 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotEmpty;
 import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class DistrictRequestDTO {
-    private BigDecimal valueDistrictM2;
     private String districtName;
+
+    @NotEmpty(message = "Valor do metro quadrado no bairro não pode estar vazio.")
+    @Digits(integer = 13, fraction = 2, message = "Valor do metro quadrado do bairro não pode estar vazio.")
+    private BigDecimal valueDistrictM2;
 }
