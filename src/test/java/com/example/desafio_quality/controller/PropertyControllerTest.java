1bd22e7733868952f7a2e3237d62ae5d59096dad
Matheus Ferreira <matheus.aferreira@mercadolivre.com>
2022-10-14 10:51:50 -0300

    test: controller getArea method test

---
+++ src/test/java/com/example/desafio_quality/controller/PropertyControllerTest.java
@@ -0,0 +1,60 @@
+package com.example.desafio_quality.controller;
+
+import com.example.desafio_quality.dto.PropertyAreaDTO;
+import com.example.desafio_quality.entity.Property;
+import com.example.desafio_quality.entity.Room;
+import com.example.desafio_quality.service.PropertyService;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(PropertyController.class)
+public class PropertyControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PropertyService service;
+
+    private PropertyAreaDTO propertyArea;
+    private Property property;
+
+    @BeforeEach
+    void setup() {
+        Room room1 = new Room(12, 12, "Quarto");
+        Room room2 = new Room(24.0, 24, "Sala");
+        List<Room> rooms = Arrays.asList(room1, room2);
+        property = new Property("teste", 1, 1, rooms);
+        propertyArea = new PropertyAreaDTO(property, 720.0);
+    }
+
+    @Test
+    void getArea_returnsArea_withCorrectPropertyId() throws Exception {
+        Mockito.when(service.getArea(ArgumentMatchers.anyInt()))
+                .thenReturn(propertyArea);
+
+        ResultActions response = mockMvc.perform(
+                get("/api/v1/properties/area/{propId}", property.getPropId())
+                        .contentType(MediaType.APPLICATION_JSON));
+
+        response.andExpect(status().isOk())
+                .andExpect(jsonPath("$.propName", CoreMatchers.is(propertyArea.getPropName())))
+                .andExpect(jsonPath("$.districtId", CoreMatchers.is(propertyArea.getDistrictId())))
+                .andExpect(jsonPath("$.roomsQnt", CoreMatchers.is(propertyArea.getRoomsQnt())))
+                .andExpect(jsonPath("$.area", CoreMatchers.is(propertyArea.getArea())));
+    }
+}
