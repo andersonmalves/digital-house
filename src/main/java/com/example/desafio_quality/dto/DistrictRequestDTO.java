5da403990c669f6d073ef83a6951489f42adc76a
gabvteixeira <gabriel.vteixeira@mercadolivre.com>
2022-10-17 10:24:42 -0300

    fix: valueDistrictM2 validation

--- src/main/java/com/example/desafio_quality/dto/DistrictRequestDTO.java
+++ src/main/java/com/example/desafio_quality/dto/DistrictRequestDTO.java
@@ -5,16 +5,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Digits;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class DistrictRequestDTO {
+
+    @NotBlank(message = "O nome do bairro não pode estar vazio")
     private String districtName;
 
-    @NotEmpty(message = "Valor do metro quadrado no bairro não pode estar vazio.")
-    @Digits(integer = 13, fraction = 2, message = "Valor do metro quadrado do bairro não pode estar vazio.")
+    @NotNull(message = "Valor do metro quadrado no bairro não pode estar vazio.")
+    @Digits(integer = 13, fraction = 2, message = "Valor do metro quadrado do bairro não pode passar 13 digitos.")
     private BigDecimal valueDistrictM2;
 }
