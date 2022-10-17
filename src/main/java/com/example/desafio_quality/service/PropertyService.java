77f67c499603c927f91ad7b60dc05882306e319b
Matheus Ferreira <matheus.aferreira@mercadolivre.com>
2022-10-17 13:40:03 -0300

    feat: Property creation tests in Service layer

--- src/main/java/com/example/desafio_quality/service/PropertyService.java
+++ src/main/java/com/example/desafio_quality/service/PropertyService.java
@@ -161,9 +161,7 @@ public class PropertyService implements IPropertyService {
                 district.getDistrictId(),
                 rooms);
 
-        this.propertyRepo.createProperty(newProperty);
-
-        return newProperty;
+        return this.propertyRepo.createProperty(newProperty);
     }
 
     /**
